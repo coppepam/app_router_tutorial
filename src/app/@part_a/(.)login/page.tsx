ba73b2ff48460abf7154edf62357015162dde570
coppe <coppepam@gmail.com>
2024-04-09 06:49:49 +0900

    Add modal for testing intercepting routes

---
+++ src/app/@part_a/(.)login/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Modal } from "@/app/ui/modal";
+import Login from "@/app/login/_login";
+
+export default function Part_A_Login() {
+  return (
+    <Modal title="test">
+      <Login />
+    </Modal>
+  );
+}
